42ca12eea9fb4b9f482ae40d273845356fb55e2e
Albert Bertram <bertrama@umich.edu>
2014-04-08 22:57:56 -0400

    Better generalizing the environment rewrites.

--- lib/support/env.rb
+++ lib/support/env.rb
@@ -5,18 +5,48 @@ module Support
     def initialize app
       @app = app
     end
+
     def call env
-      if CONFIG.has_key? :rack and CONFIG[:rack].has_key? :mount
-        env['SCRIPT_NAME'] = CONFIG[:rack][:mount] + env['SCRIPT_NAME']
+      if rewrite?
+        env['SCRIPT_NAME'] = forward env['SCRIPT_NAME']
         status, headers, body = *@app.call(env)
         if status === 301 or status === 302
           if headers.has_key? 'Location'
-            headers['Location'].sub!(%r{#{CONFIG[:rack][:mount]}/}, '/')
+            headers['Location'] = reverse headers['Location']
           end
         end
         [status, headers, body]
       else
         @app.call(env)
+      end
+    end
+
+    private
+    def rewrite?
+      ENV.has_key? 'APP_ROOT' and ENV.has_key? 'APP_CONTEXT'
+    end
+
+    def root
+      ENV['APP_ROOT'] || ''
+    end
+
+    def context
+      ENV['APP_CONTEXT'] || ''
+    end
+
+    def forward path
+      if path.index(context) === 0
+        root + path[context.length, path.length]
+      else
+        path
+      end
+    end
+
+    def reverse path
+      if path.include?(root + '/')
+        path.sub(%r{#{root + '/'}}, context + '/')
+      else
+        path
       end
     end
   end
